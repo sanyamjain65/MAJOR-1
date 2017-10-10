4a6f79e93b6895a956dc7059282abdb88265d523
vards123 <vards108216@gmail.com>
2017-10-10 16:16:30 +0530

    first commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html',
+  
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController) {
+  }
+
+  
+  gender:string;
+    javascript = false;
+    angular = false;
+    csharp = false;
+    name = 'Two way bound';
+	firstName = false;
+	lastName= false;
+  
+  
+}
